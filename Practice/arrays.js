b886dee11b7f78129834b8fc3c72200c46f58616
Syed Rafay <rafay_syed1@yahoo.com>
2016-10-15 17:13:00 +0500

    checking out JS arrays

---
+++ Practice/arrays.js
@@ -0,0 +1,11 @@
+var names=[];
+names[2]="Elliot";
+
+
+names.length;
+names.pop();
+names.push("Mr. Robot");
+
+// undefined indeces are also counted in .length method..
+// Example ; names[undefined × 2, "Mr. Robot"]
+// names.length would be 3
