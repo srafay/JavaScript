787b3ebc21341f44d46262909ec5a10bfb23d74d
Syed Rafay <rafay_syed1@yahoo.com>
2016-10-15 16:35:41 +0500

    comparison operators in detail

---
+++ Practice/comparisonOperators.js
@@ -0,0 +1,27 @@
+console.log ("if ('mark' == 'mark')");
+console.log (('mark' == "mark"));
+
+
+console.log('if ("mark" == "Mark")');
+console.log(("mark" == "Mark"));
+
+console.log('if ("Mark" === "mark")');
+console.log(("Mark" === "mark"));
+
+console.log('if ("123" == 123)');
+console.log(("123" == 123));
+
+console.log('if ("321" === 321)');
+console.log(("321" === 321));
+
+console.log('if (123 != "123")');
+console.log((123 != "123"));
+
+console.log('if ("786" !== 786)');
+console.log(("786" !== 786));
+
+console.log('if (0 <= "0")');
+console.log((0 <= "0"));
+
+console.log('if (0 == "0")');
+console.log((0 == "0"));
