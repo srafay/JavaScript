30da66b8d28a1e4af723aa38f4309edde31c062a
Syed Rafay <rafay_syed1@yahoo.com>
2016-10-15 16:14:55 +0500

    trying prompt syntax in detail

--- Practice/prompt.js
+++ Practice/prompt.js
@@ -1,3 +1,15 @@
 var userName = "user123"
 userName = window.prompt("Enter your user name",userName);
 console.log(userName);
+
+var number1;
+number1 = prompt("Enter number 1");
+var res1 = number1+1;
+console.log (number1+1);
+alert(res1);
+
+var number2;
+number2 = window.prompt("Enter number 2");
+var res2 = number2+1;
+alert(res2);
+console.log (number2+2);
